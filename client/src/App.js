c341da6c51015d0444991e1c6f28da84cc6270c6
svetstufff <63481479+svetstufff@users.noreply.github.com>
2021-10-07 15:28:29 -0700

    Initial setup of React application

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://miro.medium.com/max/1024/0*4ty0Adbdg4dsVBo3.png" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello world!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
