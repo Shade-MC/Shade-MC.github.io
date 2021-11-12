32486e3b90101f0b7adf51a95ddb825d7c362c5c
Shade-MC <554scout@gmail.com>
2021-11-11 18:44:34 -0800

    added code from mongoDB tutorial to be modified to store systematic review records

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
 import './App.css';
 
+import React from "react";
+
+// We use Route in order to define the different routes of our application
+import { Route } from "react-router-dom";
+
+// We import all the components we need in our app
+import Navbar from "./components/navbar";
+import Edit from "./components/edit";
+import Create from "./components/create";
+import RecordList from "./components/recordList";
+
+/*
 function App() {
   return (
     <div className="App">
@@ -20,5 +32,21 @@ function App() {
     </div>
   );
 }
+*/
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Route exact path="/">
+        <RecordList />
+      </Route>
+      <Route path="/edit/:id" component={Edit} />
+      <Route path="/create">
+        <Create />
+      </Route>
+    </div>
+  );
+};
 
 export default App;
